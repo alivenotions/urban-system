1f51a846e0eb18fa2dba851091d686d7e33cb347
Bhavdeep Dhanjal <bhavdeep@hey.com>
2022-12-08 04:25:27 +0100

    test on a random port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 // Use the following command for checking the generated code
 // cargo expand --test health_check
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let addr = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:9091/health_check")
+        .get(&format!("{}/health_check", &addr))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -15,7 +17,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = urban_system::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = urban_system::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
