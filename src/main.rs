2cc2dad0ee73df9bc383d0b2a45c48e8c03e39ac
Bhavdeep Dhanjal <bhavdeep@hey.com>
2022-12-08 04:25:27 +0100

    test on a random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
 use urban_system::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:9091").expect("Failed to bind to the port");
+    run(listener)?.await
 }
