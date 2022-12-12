d3e7b5476e8b44380921f52a335f3be90446cf91
Bhavdeep Dhanjal <bhavdeep@hey.com>
2022-12-12 02:32:39 +0100

    persist subscriber's email

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use urban_system::configuration::get_configuration;
 use urban_system::startup::run;
@@ -5,7 +6,10 @@ use urban_system::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to DB");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to the port");
-    run(listener)?.await
+    run(listener, connection)?.await
 }
