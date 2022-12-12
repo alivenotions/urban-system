a8661f4945d60913c03e9202b91d105e63460b79
Bhavdeep Dhanjal <bhavdeep@hey.com>
2022-12-12 22:00:04 +0100

    add logs to handlers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use urban_system::configuration::get_configuration;
@@ -5,6 +6,7 @@ use urban_system::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
